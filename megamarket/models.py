7f02e06fb3b81110bfac1bcaf8ed35b11f8f9634
nikitakosatka <nikitakosatka@yandex.ru>
2022-06-26 19:05:38 +0300

    ShopUnitStatistic Model

--- megamarket/models.py
+++ megamarket/models.py
@@ -20,3 +20,20 @@ class ShopUnit(Base):
     price = Column(Integer, nullable=True)
     date = Column(TIMESTAMP(timezone=False), nullable=False)
     children = relationship('ShopUnit', cascade="all, delete", lazy='joined')
+
+
+class ShopUnitStatistic(Base):
+    __tablename__ = 'shop_unit_statistics'
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid4,
+                unique=True, nullable=False)
+    item_id = Column(UUID(as_uuid=True), default=uuid4, nullable=False)
+    name = Column(String, nullable=False)
+    type = Column(ENUM(ShopUnitType), nullable=False)
+    item_parent_id = Column(UUID(as_uuid=True), nullable=True)
+    parent_id = Column(UUID(as_uuid=True),
+                       ForeignKey('shop_unit_statistics.id'),
+                       nullable=True)
+    price = Column(Integer, nullable=True)
+    date = Column(TIMESTAMP(timezone=False), nullable=False)
+    children = relationship('ShopUnitStatistic', cascade="all, delete")
